1156b3d0046dd4c7c1af9583b66928a70981477d
rvmelo <robertomelo7@hotmail.com>
2021-02-24 22:53:49 -0300

    refactor:add default export from container directories

--- cinemapp/src/routes/index.tsx
+++ cinemapp/src/routes/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import SearchPage from '../pages/searchPage/container';
-import FavoritesPage from '../pages/favoritesPage/container';
+import SearchPage from '../pages/searchPage';
+import FavoritesPage from '../pages/favoritesPage';
 
 const Routes: React.FC = () => (
   <Switch>
